a37cfd6d6e0746c44e78b386b4a366991982140a
David Kitz <david.kitzwork@gmail.com>
2021-12-22 12:43:57 +0100

    added grid system for response from imdb

--- js/index.js
+++ js/index.js
@@ -3,16 +3,22 @@ const divResponse = document.getElementById("searchResult");
 async function imdbApiCall() {
     let request = await fetch('https://imdb-api.com/en/API/Top250Movies/k_ei4ys9ee');
     let response = await request.json();
+
     for (let i = 0; i < 20; i++) {
-        //const div = document.createElement("div");
         const img = document.createElement("img");
+        const div = document.createElement("div");
+        div.classList.add("custom-div")
         img.src = response["items"][i]["image"];
-        img.classList.add("col-sm")
-        divResponse.append(img);
+        img.classList.add("img-thumbnail");
+        //img.classList.add("custom-image");
+        div.append(img);
+        divResponse.append(div);
         console.log(response["items"][i]);
     }
+    
+    
 
 
-    divResponse(response);
+    
 }
 imdbApiCall();
